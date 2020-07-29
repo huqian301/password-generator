6713fa88a1b19e0d85da644f1d2b3310de0cec0e
huqian301 <huqian301@gmail.com>
2020-07-28 17:09:55 -0700

    make a outline

---
+++ script.js
@@ -0,0 +1,56 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+var start = confirm("Are you ready to create your own secure password?");
+
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+if (start === ture){
+
+  // WHEN prompted for the length of the password (enter numbers only)
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+var passwordLength = prompt( "Choose your length of password, at least 8 characters and no more than 128 characters." );
+
+}
+
+
+// WHEN prompted for character types to include in the password ()
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+
+var criteria = [
+  {q:'Do you want lowercase in it?', a:ture},
+  {q:'Do you want uppercase in it?', a:ture},
+  {q:'Do you want numbers in it?', a:ture},
+  {q:'Do you want special characters in it?', a:ture}
+];
+
+for (var i = 0; i < criteria.length; i++){
+  var answer = confirm(criteria[i].q);
+}
+
+
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
